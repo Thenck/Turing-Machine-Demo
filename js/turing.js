ce32a08b54c85e03a175260d778c01a9b07c06ba
Thenck <645268434@qq.com>
2019-08-08 16:39:08 +0800

    add code

--- js/turing.js
+++ js/turing.js
@@ -34,14 +34,15 @@ $(function(){
 	})
 
 	$(".lock-program").on("click",function(){
-		$(".program-table").find("input").attr("readonly","readonly");
-		$(".program-table").find("select").attr("disabled","disabled");
-		$(".init-input-table").find("input").attr("readonly","readonly");
-		$(".init-input-table").find("select").attr("disabled","disabled");
-		
-		$(this).hide();
-		$(".unlock-program").show();
-		lockStatus = true;
+		if(lockCheck()){
+			$(".program-table").find("input").attr("readonly","readonly");
+			$(".program-table").find("select").attr("disabled","disabled");
+			$(".init-input-table").find("input").attr("readonly","readonly");
+			$(".init-input-table").find("select").attr("disabled","disabled");
+			$(this).hide();
+			$(".unlock-program").show();
+			lockStatus = true;
+		}
 	})
 	
 	$(".unlock-program").on("click",function(){
@@ -54,3 +55,89 @@ $(function(){
 		lockStatus = false;
 	})
 })
+
+
+function lockCheck(){
+	var result = true;
+	
+	if(!checkProgramDirectives()){
+		result = false;
+	}
+	if(!checkInitInput()){
+		result = false;
+	}
+	return result;
+}
+
+function checkProgramDirectives(){
+	var result =true;
+	$(".program-table").find("tbody").find("input").each(function(){
+		if(!isNotNull($(this).val())){
+			
+			if($(this).hasClass("output-write")&&$(this).css("display")=="none"){
+				result =true;
+				$(this).removeClass("error");
+			}else{
+				result =false;
+				$(this).addClass("error");
+			}
+			
+			
+		}else{
+			$(this).removeClass("error");
+		}
+	})
+	
+	$(".program-table").find("tbody").find("select").each(function(){
+		if(!isNotNull($(this).val())){
+			result =false;
+			$(this).addClass("error");
+		}else{
+			$(this).removeClass("error");
+		}
+	})
+	return result;
+}
+
+function checkInitInput(){
+	var result =true;
+	$(".init-input-table").find("tbody").find("input").each(function(){
+		if(!isNotNull($(this).val())){
+			result =false;
+			$(this).addClass("error");
+		}else{
+			$(this).removeClass("error");
+		}
+	})
+	$(".init-input-table").find("tbody").find("select").each(function(){
+		if(!isNotNull($(this).val())){
+			result =false;
+			$(this).addClass("error");
+		}else{
+			$(this).removeClass("error");
+		}
+	})
+	return result;
+}
+
+function isNotNull(value){
+	if(typeof(value)=="undefined"|| value==undefined||value == "" ){
+		return false;
+	}
+	return true;
+}
+
+
+function turing(options){
+	
+
+	
+}
+
+function programDirectives(input,status,output,nextStatus){
+	return 
+	{input:input,
+	status:status,
+	output:output,
+	nextStatus:nextStatus}
+}
